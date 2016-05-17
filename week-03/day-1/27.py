253d0fab2f4a6eacc4dbf44ac938e760106d8c78
Szemann Peter <szemannp@gmail.com>
2016-05-17 17:16:57 +0200

    9 more excercises

---
+++ week-03/day-1/27.py
@@ -0,0 +1,12 @@
+z = 13
+# if z is between 10 and 20 print 'Sweet!'
+# if less than 10 print 'More!',
+# if more than 20 print 'Less!'
+if 10 < z < 20:
+    print("Sweet!")
+
+elif 10 > z:
+    print("More!")
+
+elif 20 < z:
+    print("Less!")
