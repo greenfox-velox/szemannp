7507eac4260cd87e500e76995f91bb84789e8222
Szemann Peter <szemannp@gmail.com>
2016-05-18 11:43:23 +0200

    removed commented while's from for excercises

--- week-03/day-1/34_for.py
+++ week-03/day-1/34_for.py
@@ -1,10 +1,5 @@
 ag = [3, 4, 5, 6, 7]
 # please double all the elements of the list
-"""i = 0
-while i < len(ag):
-    ag[i] *= 2
-    print(ag[i])
-    i += 1 """
 
 for i in range(0, len(ag)):
     ag[i] *= 2
--- week-03/day-1/36_for.py
+++ week-03/day-1/36_for.py
@@ -1,11 +1,5 @@
 ah = [3, 4, 5, 6, 7]
 # print the sum of all numbers in ah
-"""i = 0
-sumall = 0
-while i < len(ah):
-    sumall += ah[i]
-    i += 1
-print(sumall) """
 
 sumall = 0
 for i in range(0, len(ah)):
