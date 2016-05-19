78836ec5c21940c57ee092246d08a61737b1460b
Szemann Peter <szemannp@gmail.com>
2016-05-19 09:09:23 +0200

    more practice on day-2

--- week-03/day-2/35.py
+++ week-03/day-2/35.py
@@ -3,8 +3,8 @@ test = 5
 
 def factorial_function(input):
     value = 1
-    for i in range(1, input + 1, 1):
-        value = value * input
+    for i in range(1, input + 1):
+        value = value * i
     return value
 
 print(factorial_function(test))
--- week-03/day-2/36.py
+++ week-03/day-2/36.py
@@ -2,8 +2,16 @@
 
 my_list = [3, 4, 5, 6, 7]
 
-def list_reverse(input_list):
-    input_list.reverse()
-    return input_list
+# def list_reverse(input_list):
+#     input_list.reverse()
+#     return input_list
+#
+# print(list_reverse(my_list))
 
-print(list_reverse(my_list))
+def revert(input):
+    reverse = []
+    for i in range(len(input)-1, -1, -1):
+        reverse += [input[i]]
+    return reverse
+
+print(revert(my_list))
