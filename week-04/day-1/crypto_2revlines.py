3acfec8288e52b846af3c34cd8efd634d128728e
Szemann Peter <szemannp@gmail.com>
2016-05-23 17:40:38 +0200

    2 more crypto's done

--- week-04/day-1/crypto_2revlines.py
+++ week-04/day-1/crypto_2revlines.py
@@ -1,3 +1,11 @@
 # Create a method that decrypts texts/reversed_zen_lines.txt
 def decrypt(file_name):
-    pass
+    file = open(file_name)
+    text = "" 
+    for line in file:
+        sorted_line = ""
+        for chars in range(len(line.rstrip()) -1, -1, -1):
+            sorted_line += line.rstrip()[chars]
+        text += sorted_line + "\n"  
+    file.close()
+    return text
--- week-04/day-1/crypto_3revorder.py
+++ week-04/day-1/crypto_3revorder.py
@@ -1,3 +1,11 @@
 # Create a method that decrypts texts/reversed_zen_order.txt
 def decrypt(file_name):
-    pass
+    file = open(file_name)
+    sorted_file = ""
+    for line in reversed(file.readlines()):
+        ordered_line = ""
+        for i in range(0, len(line.rstrip())):
+            ordered_line += line.rstrip()[i]
+        sorted_file += ordered_line + "\n"
+    file.close()
+    return sorted_file
