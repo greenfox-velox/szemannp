b68cfc4f0e6ccee702aeb7f300c4574c36891c5f
Szemann Peter <szemannp@gmail.com>
2016-06-01 16:10:18 +0200

    add list printer method

--- week-05/day-3/todo_app/todo.py
+++ week-05/day-3/todo_app/todo.py
@@ -3,12 +3,22 @@ import sys
 class ToDo():
 
     def get_usage_info(self):
-        read_menu = open('usage.txt')
-        list_menu = read_menu.read()
-        read_menu.close()
+        file_menu = open('usage.txt')
+        list_menu = file_menu.read()
+        file_menu.close()
         return(list_menu)
+
+    def get_todo_list(self):
+        file_todo = open('todo_list.txt')
+        todo_list = file_todo.readlines()
+        print('\n')
+        for line in range(len(todo_list)):
+            print(line + 1, todo_list[line].rstrip())
+        file_todo.close()
+        return ''
 
 
 workywork = ToDo()
 
-# print(workywork.get_usage_info())
+print(workywork.get_usage_info())
+print(workywork.get_todo_list())
