177135e732c9d44c6f8552def0c3439721c0997b
Szemann Peter <szemannp@gmail.com>
2016-07-11 17:29:14 +0200

    tasks covered except error handling

--- week-09/day-1/todo.js
+++ week-09/day-1/todo.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 var app = express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
 var data = [
     {
@@ -29,15 +31,43 @@ app.get('/todos', function(req, res) {
   res.json(data);
 });
 
-
 app.get('/todos/:id', function(req, res) {
   res.json(data.filter(function (item) {
-    if (parseInt(item.id) === parseInt(req.params.id)) {
+    if (parseInt(item.id) === parseInt(+req.params.id)) {
       return item;
     }
   }));
 });
 
+app.post('/todos', jsonParser, function(req, res) {
+  var currentId = data.length;
+  currentId++;
+  var newTodo = {
+    'completed': false,
+    'id': currentId,
+    'text': req.body.text
+  };
+  data.push(newTodo);
+  res.send(newTodo);
+});
 
+app.put('/todos/:id', jsonParser, function (req, res) {
+  var toPut = data.filter(function (item) {
+    return item.id === +req.params.id;
+  }) [0];
+  toPut['completed'] = req.body['completed'];
+  toPut['text'] = req.body['text'];
+  res.json(toPut);
+});
+
+app.delete('/todos/:id', function(req, res) {
+  res.json(data.filter(function (item) {
+    if (item.id === +req.params.id) {
+      item.destroyed = 'true';
+      data.splice(data.indexOf(item), 1);
+      return item;
+    }
+  })[0]);
+});
 
 app.listen(3000);
